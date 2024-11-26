d657349470211870a5231101d6d2e365de79bd38
Bruno da Silva B. <brunooh435yt@gmail.com>
2024-11-26 16:33:10 -0300

    mod3-4/prevent-default

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,37 @@
 "use client";
 
-import { DefaultButton } from "../components/DefaultButton";
+import { FormEvent } from "react";
 
 const Page = () => {
-  const thirdButtonHandler = () => alert("Evento do botão 3.");
+  const handleFormSubmit = (
+    event:FormEvent<HTMLFormElement>
+  ) => {
+    event.preventDefault();
+    alert("Evento rolando.")
+  };
   return (
-    <div className="p-4 flex flex-col gap-6 w-96 container m-auto mt-8">
-      <DefaultButton
-        label="Clique-me! 1"
-        func={() => alert("Evento do botão 1.")}
-      />
-      <DefaultButton
-        label="Clique-me! 2"
-        func={() => alert("Evento do botão 2.")}
-      />
-      <DefaultButton label="Clique-me! 3"
-       func={thirdButtonHandler} />
+    <div className="m-4 p-5 flex flex-col gap-4 w-[400px] bg-black/30 rounded-lg">
+      <h1 className="text-2xl">Formulário</h1>
+      <form className="flex flex-col gap-3" onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          className="rounded-md px-2"
+          placeholder="Digite o seu nome..."
+        />
+        <div className="flex w-full gap-2">
+          <input type="date" className="w-2/4 rounded-md px-2 text-gray-400" />
+          <input
+            type="email"
+            className="w-2/4 rounded-md px-2"
+            placeholder="Digite o seu email..."
+          />
+        </div>
+        <input
+          type="submit"
+          value="Enviar →"
+          className="bg-blue-500 rounded-md w-fit pl-3 pr-2 py-2 self-end text-sm"
+        />
+      </form>
     </div>
   );
 };
