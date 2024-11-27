5a9c95b8bffc3e65f808381a252ae97eeaef9a3e
Bruno da Silva B. <brunooh435yt@gmail.com>
2024-11-27 17:31:17 -0300

    mod3-6/states

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,23 @@
 "use client";
 
-import { FormEvent } from "react";
+import { useState } from "react";
 
 const Page = () => {
-  const handleFormSubmit = (
-    event:FormEvent<HTMLFormElement>
-  ) => {
-    event.preventDefault();
-    alert("Evento rolando.")
+  const [count, setCount] = useState<number>(0);
+
+  const handlerClick = () => {
+    setCount(count + 1);
   };
+
   return (
-    <div className="m-4 p-5 flex flex-col gap-4 w-[400px] bg-black/30 rounded-lg">
-      <h1 className="text-2xl">Formulário</h1>
-      <form className="flex flex-col gap-3" onSubmit={handleFormSubmit}>
-        <input
-          type="text"
-          className="rounded-md px-2"
-          placeholder="Digite o seu nome..."
-        />
-        <div className="flex w-full gap-2">
-          <input type="date" className="w-2/4 rounded-md px-2 text-gray-400" />
-          <input
-            type="email"
-            className="w-2/4 rounded-md px-2"
-            placeholder="Digite o seu email..."
-          />
-        </div>
-        <input
-          type="submit"
-          value="Enviar →"
-          className="bg-blue-500 rounded-md w-fit pl-3 pr-2 py-2 self-end text-sm"
-        />
-      </form>
+    <div className="m-4 mt-32 flex">
+      <p className="py-5 px-7 bg-black/30 rounded-s-lg">{count}</p>
+      <button
+        className="py-5 px-7 bg-blue-500 rounded-e-lg cursor-pointer"
+        onClick={handlerClick}
+      >
+        +1
+      </button>
     </div>
   );
 };
