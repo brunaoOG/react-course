979023430d6fa5aac42393fd8ef680c6f739764b
Bruno da Silva B. <brunooh435yt@gmail.com>
2024-11-27 23:50:47 -0300

    mod3-10/state-updated-queue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,39 +3,25 @@
 import { useState } from "react";
 
 const Page = () => {
-  const [inputValue, setInputValue] = useState("");
+  const [count, setCount] = useState(0);
 
-  const [nickname, setNickname] = useState("");
-
-  const sendNickname = () => {
-    setNickname(inputValue);
-    setInputValue("");
+  const handlerBtnClick = () => {
+    setCount(count + 2); // 0 + 2 -> count = 0 c(arg) = 2
+    setCount((c) => c + 2); // 2 + 2 (c + 2)
+    setCount((c) => c + 2); //4 + 2
   };
 
   return (
-    <div className="flex flex-col m-4 mt-32 gap-3 p-10 w-fit">
-      <p>Digite aqui:</p>
-      <input
-        type="text"
-        className="p-3 w-[340px] rounded-lg text-black"
-        placeholder="Nome de usuário"
-        value={inputValue}
-        onChange={(e) => {
-          setInputValue(e.target.value);
-          console.log(inputValue);
-        }}
-      />
-      <button
-        className="self-end text-sm bg-blue-500 rounded py-2 px-3 hover:ring-2 transition-all"
-        onClick={sendNickname}
-      >
-        {"Confirmar novo nickname ->"}
-      </button>
-      {nickname ? (
-        <div className="m-auto">Seja bem vindo novamente, {nickname}</div>
-      ) : (
-        <></>
-      )}
+    <div className="m-10 ">
+      <div className="flex">
+        <p className="py-5 px-8 rounded-s-lg bg-black/60">{count}</p>
+        <button
+          onClick={handlerBtnClick}
+          className="p-5 bg-blue-500 rounded-e-lg"
+        >
+          +6
+        </button>
+      </div>
     </div>
   );
 };
